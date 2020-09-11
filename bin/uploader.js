1e864b095e814b5a2c5f44e661db8e48815bc6d9
Dan Grossen <grossen@adobe.com>
2020-09-11 14:51:01 -0600

    dont catch errors

--- bin/uploader.js
+++ bin/uploader.js
@@ -51,98 +51,94 @@ const UPDATE_QUERY = `
   const env = ENV_NAME || 'prod';
   const { GRAFFIAS_SERVER, IMS_HOST } = ENVIRONMENTS[env];
 
-  try {
-    const zipFile = args[0];
-    if (!zipFile) {
-      throw new Error('You must provide the zip plugin package as an argument.');
+  const zipFile = args[0];
+  if (!zipFile) {
+    throw new Error('You must provide the zip plugin package as an argument.');
+  }
+  const zipPath = path.resolve(zipFile);
+  if (!/.*\.zip$/.test(zipFile) || !fs.existsSync(zipPath)) {
+    throw new Error(`No zip file found at ${zipPath}`);
+  }
+
+  const descriptor = await readPluginJsonFromPackage(zipPath);
+
+  const { namespace, version } = descriptor;
+
+  const tokenResponseJson = await fetchAccessToken({
+    CLIENT_SECRET,
+    IMS_PASSWORD,
+    IMS_HOST,
+    IMS_USERNAME
+  });
+
+  const queryPluginsResponse = await fetch(GRAFFIAS_SERVER, {
+    method: 'POST',
+    headers: {
+      'x-gw-ims-org-id': IMS_ORG,
+      'x-gw-ims-user-id': tokenResponseJson.userId,
+      'x-api-key': 'NovaTestToken',
+      Authorization: `Bearer ${tokenResponseJson.access_token}`,
+      Accept: 'application/json; version=1',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: `
+        query name($namespace: String){
+          plugins (namespace:$namespace){
+            uuid
+            namespace
+            version
+          }
+        }`,
+      variables: { namespace }
+    })
+  });
+
+  const plugins = await queryPluginsResponse.json();
+
+  if (!plugins || !plugins.data || !plugins.data.plugins) {
+    throw new Error('There was a problem fetching plugins from the server.');
+  }
+
+  const foundPlugin = plugins.data.plugins.find(plugin => plugin.namespace === namespace);
+
+  if (foundPlugin) {
+    if (semver.lte(version, foundPlugin.version)) {
+      throw new Error(`Plugin version: ${foundPlugin.version} needs to be greater than version: ${version} on the server`);
     }
-    const zipPath = path.resolve(zipFile);
-    if (!/.*\.zip$/.test(zipFile) || !fs.existsSync(zipPath)) {
-      throw new Error(`No zip file found at ${zipPath}`);
-    }
+    console.log(`Found an existing plugin with the namesapce: ${namespace}. The plugin will be updated.`);
+  }
 
-    const descriptor = await readPluginJsonFromPackage(zipPath);
+  const field = foundPlugin ? 'updatePlugin' : 'createPlugin';
+  const query = foundPlugin ? UPDATE_QUERY : CREATE_QUERY;
+  const variables = foundPlugin ? { uuid: foundPlugin.uuid, file: null } : { file: null };
 
-    const { namespace, version } = descriptor;
+  const file = await fs.createReadStream(zipPath);
 
-    const tokenResponseJson = await fetchAccessToken({
-      CLIENT_SECRET,
-      IMS_PASSWORD,
-      IMS_HOST,
-      IMS_USERNAME
-    });
+  const formData = new FormData();
+  formData.append('operations', JSON.stringify({ query, variables }));
+  formData.append('map', JSON.stringify({ 0: ['variables.file'] }));
+  formData.append('0', file);
 
-    const queryPluginsResponse = await fetch(GRAFFIAS_SERVER, {
-      method: 'POST',
-      headers: {
-        'x-gw-ims-org-id': IMS_ORG,
-        'x-gw-ims-user-id': tokenResponseJson.userId,
-        'x-api-key': 'NovaTestToken',
-        Authorization: `Bearer ${tokenResponseJson.access_token}`,
-        Accept: 'application/json; version=1',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        query: `
-          query name($namespace: String){
-            plugins (namespace:$namespace){
-              uuid
-              namespace
-              version
-            }
-          }`,
-        variables: { namespace }
-      })
-    });
+  const uploadPluginResponse = await fetch(GRAFFIAS_SERVER, {
+    method: 'POST',
+    headers: {
+      'x-gw-ims-org-id': IMS_ORG,
+      'x-gw-ims-user-id': tokenResponseJson.userId,
+      'x-api-key': 'NovaTestToken',
+      Authorization: `Bearer ${tokenResponseJson.access_token}`,
+      ...formData.getHeaders()
+    },
+    body: formData
+  });
 
-    const plugins = await queryPluginsResponse.json();
+  const uploadedPlugin = await uploadPluginResponse.json();
+  const uuid = uploadedPlugin.data && uploadedPlugin.data[field]
+    ? uploadedPlugin.data[field].uuid : null;
 
-    if (!plugins || !plugins.data || !plugins.data.plugins) {
-      throw new Error('There was a problem fetching plugins from the server.');
-    }
-
-    const foundPlugin = plugins.data.plugins.find(plugin => plugin.namespace === namespace);
-
-    if (foundPlugin) {
-      if (semver.lte(version, foundPlugin.version)) {
-        throw new Error(`Plugin version: ${foundPlugin.version} needs to be greater than version: ${version} on the server`);
-      }
-      console.log(`Found an existing plugin with the namesapce: ${namespace}. The plugin will be updated.`);
-    }
-
-    const field = foundPlugin ? 'updatePlugin' : 'createPlugin';
-    const query = foundPlugin ? UPDATE_QUERY : CREATE_QUERY;
-    const variables = foundPlugin ? { uuid: foundPlugin.uuid, file: null } : { file: null };
-
-    const file = await fs.createReadStream(zipPath);
-
-    const formData = new FormData();
-    formData.append('operations', JSON.stringify({ query, variables }));
-    formData.append('map', JSON.stringify({ 0: ['variables.file'] }));
-    formData.append('0', file);
-
-    const uploadPluginResponse = await fetch(GRAFFIAS_SERVER, {
-      method: 'POST',
-      headers: {
-        'x-gw-ims-org-id': IMS_ORG,
-        'x-gw-ims-user-id': tokenResponseJson.userId,
-        'x-api-key': 'NovaTestToken',
-        Authorization: `Bearer ${tokenResponseJson.access_token}`,
-        ...formData.getHeaders()
-      },
-      body: formData
-    });
-
-    const uploadedPlugin = await uploadPluginResponse.json();
-    const uuid = uploadedPlugin.data && uploadedPlugin.data[field]
-      ? uploadedPlugin.data[field].uuid : null;
-
-    if (uuid) {
-      console.log(`Uploaded plugin uuid: ${uuid}`);
-    } else {
-      console.log(`There was a problem uploading the plugin: ${JSON.stringify(uploadedPlugin)}`);
-    }
-  } catch (err) {
-    console.log(`${err}`);
+  if (uuid) {
+    console.log(`Uploaded plugin uuid: ${uuid}`);
+  } else {
+    throw new Error(`There was a problem uploading the plugin: ${JSON.stringify(uploadedPlugin)}`);
   }
 })();
